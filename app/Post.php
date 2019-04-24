7c7b5b84912754cf7ede3a333d17041bfc428ba7
Azamat Vakhobov <azamat9812@gmail.com>
2019-04-24 21:43:57 +0500

    finish untill 4

--- app/Post.php
+++ app/Post.php
@@ -2,53 +2,13 @@
 
 namespace App;
 
-class Post
-{
-    public function getPosts($session)
-    {
-        if (!$session->has('posts')) {
-            $this->createDummyData($session);
-        }
-        return $session->get('posts');
-    }
+use Illuminate\Database\Eloquent\Model;
 
-    public function getPost($session, $id)
-    {
-        if (!$session->has('posts')) {
-            $this->createDummyData();
-        }
-        return $session->get('posts')[$id];
-    }
+class Post extends Model {
+    protected $fillable = ['title', 'content'];
 
-    public function addPost($session, $title, $content)
+    public function likes()
     {
-        if (!$session->has('posts')) {
-            $this->createDummyData();
-        }
-        $posts = $session->get('posts');
-        array_push($posts, ['title' => $title, 'content' => $content]);
-        $session->put('posts', $posts);
-    }
-
-    public function editPost($session, $id, $title, $content)
-    {
-        $posts = $session->get('posts');
-        $posts[$id] = ['title' => $title, 'content' => $content];
-        $session->put('posts', $posts);
-    }
-
-    private function createDummyData($session)
-    {
-        $posts = [
-            [
-                'title' => 'Learning Laravel',
-                'content' => 'This blog post will get you right on track with Laravel!'
-            ],
-            [
-                'title' => 'Something else',
-                'content' => 'Some other content'
-            ]
-        ];
-        $session->put('posts', $posts);
+        return $this->hasMany('App\Like', 'post_id');
     }
 }
--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <form action="{{ route('admin.update') }}" method="post">
+                {{ csrf_field() }}
                 <div class="form-group">
                     <label for="title">Title</label>
                     <input
@@ -12,7 +13,7 @@
                             class="form-control"
                             id="title"
                             name="title"
-                            value="{{ $post['title'] }}">
+                            value="{{ $post->title }}">
                 </div>
                 <div class="form-group">
                     <label for="content">Content</label>
@@ -21,9 +22,8 @@
                             class="form-control"
                             id="content"
                             name="content"
-                            value="{{ $post['content'] }}">
+                            value="{{ $post->content }}">
                 </div>
-                {{ csrf_field() }}
                 <input type="hidden" name="id" value="{{ $postId }}">
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
